31e9eb7126b8c038416efe38c81b3d34c34d0491
alexy4744 <alexy4744@gmail.com>
2018-11-15 21:32:59 -0500

    rework the style of the whole page and add language fetching for repos

--- index.js
+++ index.js
@@ -1,25 +1,76 @@
+/* eslint no-undef: 0 */
+
+const projects = document.getElementById("projects");
+
+(async () => {
+  const repositories = await getRepositories().catch(error => ({ error }));
+  if (repositories.error) return console.error(repositories.error);
+
+  for (const repository of repositories) {
+    if (repository.archived || repository.fork || repository.private) continue; // Skip this repository if any of these are true
+
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const p = document.createElement("p");
+
+    div.className = "repository";
+
+    a.className = "repository-name";
+    a.innerHTML = repository.name;
+    a.href = repository.html_url;
+
+    p.className = "description";
+    p.innerHTML = repository.description;
+
+    div.appendChild(a);
+    div.appendChild(p);
+
+    const languages = await getLanguages(repository.name).catch(error => ({ error }));
+
+    if (languages.error || Object.keys(languages).length < 1) {
+      projects.appendChild(div);
+      continue;
+    }
+
+    const languagesDiv = document.createElement("div"); // Container to hold all the languages
+    languagesDiv.className = "languages";
+
+    for (let language in languages) {
+      const lang = language;
+
+      language = document.createElement("div"); // Container to hold all the text and color of the language
+      language.className = "language";
+
+      const langName = document.createElement("span");
+      langName.className = "language-name";
+      langName.innerHTML = lang;
+
+      const langColor = document.createElement("div");
+      langColor.className = `language-color ${lang.toLowerCase()}`;
+
+      language.appendChild(langColor);
+      language.appendChild(langName);
+
+      languagesDiv.appendChild(language);
+      div.appendChild(languagesDiv);
+    }
+
+    projects.appendChild(div);
+  }
+})();
+
 async function getRepositories() {
-  const projects = await superagent.get("https://api.github.com/users/alexy4744/repos").catch(error => ({ error }))
-  if (projects.error) return Promise.reject(projects.error)
-  return Promise.resolve(projects.body)
+  const repositories = await superagent
+    .get("https://api.github.com/users/alexy4744/repos")
+    .catch(error => ({ error }));
+  if (repositories.error) return Promise.reject(repositories.error);
+  return Promise.resolve(repositories.body);
 }
 
-const forks = document.getElementById("forks");
-const owner = document.getElementById("owner");
-
-getRepositories()
-  .then(projects => {
-    for (const project of projects) {
-      const div = document.createElement("div");
-      const a = document.createElement("a");
-
-      a.innerHTML = project.name
-      a.href = project.html_url
-
-      div.appendChild(a);
-
-      if (project.fork) forks.appendChild(div)
-      else owner.appendChild(div)
-    }
-  })
-  .catch(error => console.error(error));
+async function getLanguages(repositoryName) {
+  const languages = await superagent
+    .get(`https://api.github.com/repos/alexy4744/${repositoryName}/languages`)
+    .catch(error => ({ error }));
+  if (languages.error) return Promise.reject(languages.error);
+  return Promise.resolve(languages.body);
+}
