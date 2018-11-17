3b27baa9d10be8c26b61e780af33ac9065c3f5e8
alexy4744 <alexy4744@gmail.com>
2018-11-17 00:06:52 -0500

    add changeable background colors

---
+++ assets/js/colors.js
@@ -0,0 +1,42 @@
+const colors = { // eslint-disable-line
+  "red": {
+    "red-dark": "#cc1f1a",
+    "red": "#e3342f",
+    "red-light": "#ef5753"
+  },
+  "orange": {
+    "orange-dark": "#de751f",
+    "orange": "#f6993f",
+    "orange-light": "#faad63"
+  },
+  "green": {
+    "green-dark": "#1f9d55",
+    "green": "#38c172",
+    "green-light": "#51d88a"
+  },
+  "teal": {
+    "teal-dark": "#38a89d",
+    "teal": "#4dc0b5",
+    "teal-light": "#64d5ca"
+  },
+  "blue": {
+    "blue-dark": "#2779bd",
+    "blue": "#3490dc",
+    "blue-light": "#6cb2eb"
+  },
+  "indigo": {
+    "indigo-dark": "#5661b3",
+    "indigo": "#6574cd",
+    "indigo-light": "#7886d7"
+  },
+  "purple": {
+    "purple-dark": "#794acf",
+    "purple": "#9561e2",
+    "purple-light": "#a779e9"
+  },
+  "pink": {
+    "pink-dark": "#eb5286",
+    "pink": "#f66d9b",
+    "pink-light": "#fa7ea8"
+  }
+};
--- assets/js/index.js
+++ assets/js/index.js
@@ -9,20 +9,48 @@ function initialize() {
   renderRepositories(); // Fetch repositories from GitHub API
 }
 
-async function getRepositories() {
-  const repositories = await superagent
-    .get("https://api.github.com/users/alexy4744/repos")
-    .catch(error => ({ error }));
-  if (repositories.error) return Promise.reject(repositories.error);
-  return Promise.resolve(repositories.body);
+function calculateAge() {
+  const born = new Date("April 30, 2002 00:00:00");
+  const age = Math.floor((Date.now() - born) * 3.17098e-11); // Find the difference between the current date with my birthday and convert to years
+
+  document.getElementById("about-me").innerHTML = document.getElementById("about-me").innerHTML.replace("${age}", age);
 }
 
-async function getLanguages(repositoryName) {
-  const languages = await superagent
-    .get(`https://api.github.com/repos/alexy4744/${repositoryName}/languages`)
-    .catch(error => ({ error }));
-  if (languages.error) return Promise.reject(languages.error);
-  return Promise.resolve(languages.body);
+function hoverableLogos() {
+  const logos = document.getElementsByClassName("logo");
+
+  for (const logo of logos) {
+    logo.setAttribute("onmouseenter", "colorizeLogo(this)");
+    logo.setAttribute("onmouseleave", "muteLogo(this)");
+  }
+}
+
+function colorizeLogo(logo) { // eslint-disable-line
+  const parts = document.getElementsByClassName(logo.attributes.logo.value); // Get any other parts of the logo
+  for (const part of parts) part.setAttribute("style", `fill: ${part.attributes.color.value} !important;`); // Set each part to their designated colors specified in the attribute
+}
+
+function muteLogo(logo) { // eslint-disable-line
+  const parts = document.getElementsByClassName(logo.attributes.logo.value); // Get any other parts of the logo
+  for (const part of parts) part.setAttribute("style", `fill: ${part.attributes.mute.value} !important`); // Revert the colors back to specified values
+}
+
+function changePrimary() { // eslint-disable-line
+  const html = document.getElementsByTagName("html")[0];
+  const chosenPalette = Object.keys(colors)[Math.floor(Math.random() * Object.keys(colors).length)];
+
+  for (const color in colors[chosenPalette]) {
+    const shade = color.split("-")[1];
+    if (!checkPalette(shade, colors[chosenPalette][color])) return changePrimary(); // Make sure the chosen palette is not the current one that is already applied by calling itself again if false.
+    if (shade) html.style.setProperty(`--primary-${shade}`, colors[chosenPalette][color]);
+    else html.style.setProperty("--primary", colors[chosenPalette][color]);
+  }
+
+  function checkPalette(shade, color) {
+    const value = getComputedStyle(document.body).getPropertyValue(`--primary${`-${shade}` || ""}`);
+    if (value === color) return false;
+    return true;
+  }
 }
 
 async function renderRepositories() {
@@ -88,28 +116,18 @@ async function renderRepositories() {
   }
 }
 
-function hoverableLogos() {
-  const logos = document.getElementsByClassName("logo");
-
-  for (const logo of logos) {
-    logo.setAttribute("onmouseenter", "colorizeLogo(this)");
-    logo.setAttribute("onmouseleave", "muteLogo(this)");
-  }
+async function getRepositories() {
+  const repositories = await superagent
+    .get("httaps://api.github.com/users/alexy4744/repos")
+    .catch(error => ({ error }));
+  if (repositories.error) return Promise.reject(repositories.error);
+  return Promise.resolve(repositories.body);
 }
 
-function colorizeLogo(logo) { // eslint-disable-line
-  const parts = document.getElementsByClassName(logo.attributes.logo.value); // Get any other parts of the logo
-  for (const part of parts) part.setAttribute("style", `fill: ${part.attributes.color.value} !important;`); // Set each part to their designated colors specified in the attribute
-}
-
-function muteLogo(logo) { // eslint-disable-line
-  const parts = document.getElementsByClassName(logo.attributes.logo.value); // Get any other parts of the logo
-  for (const part of parts) part.setAttribute("style", `fill: ${part.attributes.mute.value} !important`); // Revert the colors back to specified values
-}
-
-function calculateAge() {
-  const born = new Date("April 30, 2002 00:00:00");
-  const age = Math.floor((Date.now() - born) * 3.17098e-11); // Find the difference between the current date with my birthday and convert to years
-
-  document.getElementById("about-me").innerHTML = document.getElementById("about-me").innerHTML.replace("${age}", age);
+async function getLanguages(repositoryName) {
+  const languages = await superagent
+    .get(`https://api.github.com/repos/alexy4744/${repositoryName}/languages`)
+    .catch(error => ({ error }));
+  if (languages.error) return Promise.reject(languages.error);
+  return Promise.resolve(languages.body);
 }
