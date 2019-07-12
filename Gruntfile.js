397021f812bc4cd71121f00f807fc4f690af053e
alexy4744 <alexy4744@gmail.com>
2019-07-12 13:40:01 -0400

    remove rollup and babel from grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const autoprefixer = require("autoprefixer");
-const babel = require("rollup-plugin-babel");
 const cssnano = require("cssnano");
-const nodeBuiltins = require("rollup-plugin-node-builtins");
 const sass = require("sass");
 
 module.exports = (grunt) => {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    rollup: {
-      options: {
-        plugins: () => [
-          nodeBuiltins(),
-          babel({
-            exclude: "./node_modules/**",
-            presets: ["@babel/preset-env"],
-            sourceMap: false
-          })
-        ]
-      },
-      files: {
-        dest: "public/js/main.bundle.js",
-        src: "src/assets/js/main.js",
-      },
-    },
     sass: {
       options: {
         implementation: sass,
@@ -56,18 +38,13 @@ module.exports = (grunt) => {
       css: {
         files: "./src/assets/scss/**/*.scss",
         tasks: ["sass"]
-      },
-      js: {
-        files: "./src/assets/js/**/*.js",
-        tasks: ["rollup"],
       }
     }
   });
 
   grunt.loadNpmTasks("grunt-postcss");
-  grunt.loadNpmTasks("grunt-rollup");
   grunt.loadNpmTasks("grunt-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
 
-  grunt.registerTask("default", ["rollup", "sass", "postcss"]);
+  grunt.registerTask("default", ["sass", "postcss"]);
 };
